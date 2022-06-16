a6b5d5ad138edf434feb8543bfc763d234a66a90
kohj1018 <kohj1018@gmail.com>
2022-06-17 05:47:35 +0900

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 이렇게 된 데이터를 분석해서 가져올 수 있게 해주는 것
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json 으로 된 데이터를 분석해서 가져올 수 있게 해주는 것
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://KBW:1234@boilerplate.phofb.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요'));
+
+app.post('/register', (req, res) => {
+  // 회원 가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
